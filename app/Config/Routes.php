e2b526f545e8b53b74d5460b55b56f3b911ef75b
Alekssandher <94875660+Alekssandher@users.noreply.github.com>
2026-02-23 12:15:17 -0300

    update: libs version test

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,10 +11,11 @@ $routes = Services::routes();
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Site');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
+
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
